c5b8908d601734408959e4e2c424896fa996fd78
justinkimm17 <183580285+DontUseCheats@users.noreply.github.com>
2026-08-05 20:13:55 -0700

    Add CI/CD Github Actions workflow and unit tests

--- test_app.py
+++ test_app.py
@@ -28,6 +28,14 @@ class TestApp(unittest.TestCase):
 
     @patch('main.get_connection')
     def test_get_route(self, mock_get_connection):
+        # configuring the fake function
+        mock_conn = MagicMock()
+        mock_get_connection.return_value = mock_conn
+        mock_conn.cursor().fetchone.return_value = ('https://www.google.com',)
 
+        # send GET request
+        response = self.client.get('/abc123')
 
-    def test_short_code_length(self):
+        # assert results test
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('https://www.google.com', response.headers['Location'])
